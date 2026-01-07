5cd33e6c3179fe185402837b6e8229051b60e17a
iachb <ivanchavezbrb@gmail.com>
2026-01-06 19:15:30 -0600

    Defining product entity

---
+++ backend/src/Core/Ecommerce.Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Common;
+
+namespace Ecommerce.Domain
+{
+    public class Product : BaseDomainModel
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public int Rating { get; set; }
+        public string? Vendor { get; set; }
+        public int Stock { get; set; }
+        public ProductStatus Status { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ backend/src/Core/Ecommerce.Domain/ProductStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ecommerce.Domain
+{
+    public enum ProductStatus
+    {
+        [EnumMember(Value = "Inactive Product")]
+        Inactive,
+        [EnumMember(Value = "Active Product")]
+        Active
+    }
+}
