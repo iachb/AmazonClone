015e36a953ae54a47c9dc48728c5c71ff5ff1aa5
iachb <ivanchavezbrb@gmail.com>
2026-01-10 14:24:22 -0600

    Persistence and Unit of Work implementation

---
+++ backend/src/Core/Ecommerce.Application/Persistence/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Ecommerce.Application.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class;
+        Task<int> Complete();
+    }
+}
