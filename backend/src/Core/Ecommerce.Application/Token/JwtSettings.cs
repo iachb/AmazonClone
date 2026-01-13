aa220f9e8c4daed6117d06f4e9804f4f380ea662
iachb <ivanchavezbrb@gmail.com>
2026-01-13 13:03:17 -0600

    Implementing service registration for infrastructure

---
+++ backend/src/Core/Ecommerce.Application/Token/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Application.Token
+{
+    public class JwtSettings
+    {
+        public string? Property { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public double DurationInMinutes { get; set; }
+        public TimeSpan ExpireTime { get; set; }
+    }
+}
---
+++ backend/src/Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Application.Persistence;
+using Ecommerce.Application.Token;
+using Ecommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            return services;
+        }
+    }
+}
