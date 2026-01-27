46e4ba12516b8ad2ea8322c85941ed74b4508eb8
iachb <ivanchavezbrb@gmail.com>
2026-01-26 21:11:20 -0600

    Middlware exceptions implementation

---
+++ backend/src/Api/Errors/CodeErrorResponse.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Ecommerce.Api.Errors
+{
+    public class CodeErrorResponse
+    {
+        [JsonProperty(PropertyName = "statusCode")]
+        public int StatusCode { get; set; }
+        [JsonProperty(PropertyName = "message")]
+        public string[]? Message { get; set; }
+
+        public CodeErrorResponse(int statusCode, string[]? message = null)
+        {
+            StatusCode = statusCode;
+            if (message != null)
+            {
+                Message = new string[0];
+                var text = GetDefaultMessageStatusCode(statusCode);
+                Message[0] = text;
+            }
+            else
+            {
+                Message = message;
+            }
+        }
+
+        private string GetDefaultMessageStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                403 => "Forbidden",
+                404 => "Not Found",
+                500 => "Internal Server Error",
+                _ => "Error"
+            };
+        }
+    }
+}
